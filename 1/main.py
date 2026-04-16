2d4484eecc5db608a80d71f7e21a137c024d8df0
Tariq Kamardeen <tariq.kamardeen@gendigital.com>
2026-04-16 10:15:30 +0530

    ruff format existing files

--- 1/main.py
+++ 1/main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+
 def main():
-  sum = 0
-  for i in range(1, 1000):
-    if i % 3 == 0 or i % 5 == 0:
-      sum += i
-  print(sum)
+    sum = 0
+    for i in range(1, 1000):
+        if i % 3 == 0 or i % 5 == 0:
+            sum += i
+    print(sum)
+
 
 if __name__ == "__main__":
-  main()
+    main()
