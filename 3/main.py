d18340717b8a9ed361c65f11a98034c3d1783c2c
Tariq Kamardeen <tariq.kamardeen@gendigital.com>
2026-04-17 09:12:00 +0530

    improve solution for 3 by checking upto sqrt(n)

--- 3/main.py
+++ 3/main.py
@@ -15,13 +15,18 @@ def main():
             n //= 2
 
     factor = 3
-    while n > 1:
+    max_factor = int(n**0.5) + 1
+    while n > 1 and factor <= max_factor:
         if n % factor == 0:
             n //= factor
             last_factor = factor
             while n % factor == 0:
                 n //= factor
+            max_factor = int(n**0.5) + 1
         factor += 2
+
+    if n > 1:
+        last_factor = n
 
     print(last_factor)
 
